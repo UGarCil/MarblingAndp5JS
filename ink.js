816b8ddb062678bd92850e9e0a609c28bc7f4ab7
Uriel Garcilazo <garcilazo.uriel@gmail.com>
2024-05-04 21:35:31 -0400

    initial commit p5js

--- ink.js
+++ ink.js
@@ -1,12 +1,44 @@
+const circleRES = 100;
+
 class Drop{
     constructor(x,y,r){
-        this.x = x ;
-        this.y = y ;
+        this.center = createVector(x,y);
         this.r = r ;
+        this.vertices = [];
+        for (let i = 0; i<circleRES;i++){
+            let angle = map(i,0,circleRES,0,TWO_PI);
+            let v = createVector(cos(angle),sin(angle));
+            v.mult(this.r);
+            v.add(this.center);
+            this.vertices[i] = v;
+        }
+        this.color = random(0,200)
+
+
+    }
+
+    marble(other){
+        for (let v of this.vertices){
+            let c = other.center;
+            let r = other.r;
+            let p = v.copy();
+            p.sub(c);
+            let m = p.mag();
+            let root = sqrt(1 + (r*r)/(m*m));
+            p.mult(root);
+            p.add(c);
+            v.set(p);
+
+        }
     }
 
     show(){
-        fill(0);
-        circle(this.x,this.y,this.r*2);
+        fill(this.color);
+        // circle(this.x,this.y,this.r*2);
+        beginShape();
+        for (let v of this.vertices){
+            vertex(v.x,v.y);
+        }
+        endShape(CLOSE);
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,18 +1,30 @@
 let drops = [];
+let containerWidth = document.getElementById('canvas-container').offsetWidth;
+let containerHeight = document.getElementById('canvas-container').offsetHeight;
 
-function setup(){
-    createCanvas(400,400);
+function setup() {
+    canvas = createCanvas(containerWidth, 500);
+    canvas.parent("canvas-container");
 }
 
 
-function mousePressed(){
-    let drop = new Drop(mouseX, mouseY,50);
+function mousePressed() {
+    let drop = new Drop(mouseX, mouseY, 50);
     drops.push(drop);
 
+    for (let other of drops) {
+        other.marble(drop);
+    }
 }
-function draw(){
+function draw() {
     background(33);
-    for (let drop of drops){
+    for (let drop of drops) {
         drop.show();
     }
+}
+
+function windowResized() {
+    containerWidth = document.getElementById('canvas-container').offsetWidth;
+    containerHeight = document.getElementById('canvas-container').offsetHeight;
+    resizeCanvas(containerWidth,500);
 }
